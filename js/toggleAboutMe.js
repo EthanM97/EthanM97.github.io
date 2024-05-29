07445b53320ec3b18bf7281f03b62c7cd47c4c50
Ethan Morissette <Ethan.morissette@gmail.com>
2024-05-29 12:35:55 -0700

    Done

--- js/toggleAboutMe.js
+++ js/toggleAboutMe.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $(".toggle-button").click(function() {
-        var fullContent = $(this).closest(".card").find(".full-content");
+        var card = $(this).closest(".card");
+        var fullContent = card.find(".full-content");
+        var preview = card.find(".preview");
+
         fullContent.toggleClass("show");
+        preview.toggleClass("hide");
         $(this).find("i").toggleClass("fa-plus fa-minus");
     });
 });
